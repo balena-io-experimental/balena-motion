fa5de3e30254e7d1e0c0d0e45965049e5d5c6f86
jtonello <jtonello@kingdel.tiny.lab>
2020-07-27 17:42:54 -0400

    Updated for Deploy with balena

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sed -i "s/EXPOSE_PORT/${EXPOSE_PORT}/g" /etc/motion/motion.conf
-sed -i "s/CAMERA_NAME/${CAMERA_NAME}/g" /etc/motion/motion.conf
+envsubst < motion-template.conf > motion.conf
 
-/usr/bin/motion -c /etc/motion/motion.conf
+cp motion.conf /etc/motion/motion.conf
+
+/usr/bin/motion -c motion.conf
